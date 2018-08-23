32f9b25a70bcbbd04580f86dc3cb95e098ed2969
yojinlll <yojin1995@qq.com>
2018-08-23 21:36:38 +0800

    MVC 2

--- js/init-swiper.js
+++ js/init-swiper.js
@@ -1,29 +1,28 @@
 !function (){
     var view = document.querySelector('#mySlides')
+    // view.style.border = '1px solid red'
 
-    var controller = function(view){
-        var mySwiper = new Swiper (view.querySelector('.swiper-container'), {
-            // Optional parameters
+    var controller = {
+        view:null,
+        swiper:null,
+        swiperOptions:{
             loop: true,
-    
-            // If we need pagination
-            pagination: {
-            el: '.swiper-pagination',
-            },
-    
-            // Navigation arrows
-            navigation: {
-            nextEl: '.swiper-button-next',
-            prevEl: '.swiper-button-prev',
-            },
-    
-            // And if we need scrollbar
-            scrollbar: {
-            el: '.swiper-scrollbar',
-            },
-        })
+            pagination: { el: '.swiper-pagination', },
+            navigation: { nextEl: '.swiper-button-next', prevEl: '.swiper-button-prev', },
+            scrollbar: { el: '.swiper-scrollbar', },
+        },
+        init:function(view){
+            this.view = view
+            this.initSwiper()
+        },
+        initSwiper:function(){
+            this.swiper = new Swiper (
+            this.view.querySelector('.swiper-container'), 
+            this.swiperOptions
+            ) 
+        }
     }
 
-    controller(view)
-    
+    controller.init(view)
+
 }.call()
--- js/smoothly-navigation.js
+++ js/smoothly-navigation.js
@@ -1,44 +1,53 @@
 !function (){
     var view = document.querySelector('nav.menu')
 
-    var controller = function(view){
-        //动画：滚动到指定元素内容
-        //方法一：缓动效果
-        let aTags = view.querySelectorAll('nav.menu > ul > li > a')
-        function animate(time) {
-            requestAnimationFrame(animate);
-            TWEEN.update(time);
-        }
+    // var controller = {
+    //     view:null,
+    //     init:function(view){
+    //         this.view = view
+    //         this.aTags = this.view.querySelectorAll('nav.menu > ul > li > a')
+    //     },
+    //     animate:function(time){
+    //         requestAnimationFrame(this.animate.bind(this));
+    //         TWEEN.update(time);
+    //     },
+    // }
+
+
+
+    //动画：滚动到指定元素内容
+    //方法一：缓动效果
+    let aTags = view.querySelectorAll('nav.menu > ul > li > a')
+    function animate(time) {
         requestAnimationFrame(animate);
+        TWEEN.update(time);
+    }
+    requestAnimationFrame(animate);
 
-        for(let i=0; i<aTags.length; i++){
-            aTags[i].onclick = function(x){
-            x.preventDefault()
-            let a = x.currentTarget
-            let href = a.getAttribute('href') //'#siteAbout'
-            let element = document.querySelector(href)
-            let top = element.offsetTop
+    for(let i=0; i<aTags.length; i++){
+        aTags[i].onclick = function(x){
+        x.preventDefault()
+        let a = x.currentTarget
+        let href = a.getAttribute('href') //'#siteAbout'
+        let element = document.querySelector(href)
+        let top = element.offsetTop
 
-            let currentTop = window.scrollY
-            let targetTop = top - 80
-            let s = targetTop - currentTop // 路程
-            var coords = { y: currentTop}; // 起始位置
-            var t = Math.abs((s/100)*300) // 时间
-            if(t>500){ t = 500 }
-            var tween = new TWEEN.Tween(coords) // 起始位置
-                .to({ y: targetTop}, t) // 结束位置 和 时间
-                .easing(TWEEN.Easing.Cubic.InOut) // 缓动类型
-                .onUpdate(function() {
-                // coords.y 已经变了
-                window.scrollTo(0,coords.y) // 如何更新界面
-            })
-                .start(); // 开始缓动
-            }
+        let currentTop = window.scrollY
+        let targetTop = top - 80
+        let s = targetTop - currentTop // 路程
+        var coords = { y: currentTop}; // 起始位置
+        var t = Math.abs((s/100)*300) // 时间
+        if(t>500){ t = 500 }
+        var tween = new TWEEN.Tween(coords) // 起始位置
+            .to({ y: targetTop}, t) // 结束位置 和 时间
+            .easing(TWEEN.Easing.Cubic.InOut) // 缓动类型
+            .onUpdate(function() {
+            // coords.y 已经变了
+            window.scrollTo(0,coords.y) // 如何更新界面
+        })
+            .start(); // 开始缓动
         }
     }
-
-    controller(view)
-    
 }.call()
 
 
